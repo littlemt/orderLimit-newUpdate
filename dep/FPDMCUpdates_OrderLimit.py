6ec9ee28e4bc0451a61cb9c3dd2bfaf111e45761
littlemt <koyakazzy@yahoo.com>
2023-05-08 12:34:43 -0400

    fix extend

--- dep/FPDMCUpdates_OrderLimit.py
+++ dep/FPDMCUpdates_OrderLimit.py
@@ -119,7 +119,7 @@ def changeTau(tau,tauMax,mList,pExt,order,mu,m):
     if order==0:
         t=0
     else:
-        t=mList[2*order+1,0]
+        t=mList[2*order,0]
     
     R=nrand.uniform()
     tauNew=t-np.log(R)*(abs(eps-mu))**-1
